4ab15b0daf1c01dd6d4450e5e32f69493a7ba27d
Joanatas Fernandes Barbosa <94496092+joanatasFernandes@users.noreply.github.com>
2024-11-21 08:52:36 -0300

    Criando Componente Menu lateral

--- src/components/tamplate/AreaLateral.svelte
+++ src/components/tamplate/AreaLateral.svelte
@@ -1,6 +1,23 @@
+<script>
+    import Menu from "./Menu.svelte";
+
+</script>
 <aside class="flex flex-col self-start border rounded-md border-zinc-800  p-5 gap-10 w-72">
-    
-    <span>Menu #1</span>
-    <span>Menu #2</span>
-    <span>Menu #3</span>
+    <Menu Titulo = "Nevagação">
+        <li>Teste #1</li>
+        <li>Teste #2</li>
+        <li>Teste #3</li>
+    </Menu>
+
+    <Menu Titulo = "Principais Autores">
+        <li>Teste #1</li>
+        <li>Teste #2</li>
+        <li>Teste #3</li>
+    </Menu>
+
+    <Menu Titulo = "Outras Categorias">
+        <li>Teste #1</li>
+        <li>Teste #2</li>
+        <li>Teste #3</li>
+    </Menu>
 </aside>
---
+++ src/components/tamplate/Menu.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let Titulo = "Navegação";
+</script>
+
+<div class="flex flex-col gap-3">
+    <span class="tex-sm font-bold text-zinc-500">{Titulo}</span> 
+    <ul class="flex flex-col"> 
+    <slot></slot>
+    </ul>
+</div>
