9d0d540382cb445558c440c0693043fe1e4459cc
Joanatas Fernandes Barbosa <94496092+joanatasFernandes@users.noreply.github.com>
2024-11-21 07:11:51 -0300

    Adicionando logo com Taillwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import '../app.css';
+    import Logo from '../components/tamplate/logo.svelte';
 	let { children } = $props();
 </script>
 
 <div class="flex flex-col flex justify-center items-center gap-5
 bg-black text-white min-h-screen">
+<Logo />
 {@render children()}
 </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,4 @@
-<script>
-    import Caixa from "../components/Caixa.svelte";
-
-</script>
 
 <h1 class="text=3xl font-bold"> Ordem dos Livros</h1>
 <a href="/autores">Autores</a>
-
-<Caixa />   
-<Caixa />   
-<Caixa />   
-<Caixa />   
 
