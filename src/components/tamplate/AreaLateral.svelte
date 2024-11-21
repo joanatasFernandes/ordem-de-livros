7847fd0e3dd3f9e5d3dc3cf67e60fc1308e9e717
Joanatas Fernandes Barbosa <94496092+joanatasFernandes@users.noreply.github.com>
2024-11-21 14:34:02 -0300

    Finalizando itens do menu lateral

--- src/components/tamplate/AreaLateral.svelte
+++ src/components/tamplate/AreaLateral.svelte
@@ -1,23 +1,28 @@
 <script>
     import Menu from "./Menu.svelte";
+    import MenuItem from "./MenuItem.svelte";
 
 </script>
 <aside class="flex flex-col self-start border rounded-md border-zinc-800  p-5 gap-10 w-72">
-    <Menu Titulo = "Nevagação">
-        <li>Teste #1</li>
-        <li>Teste #2</li>
-        <li>Teste #3</li>
+
+    <Menu Titulo = "Navegação">
+        <MenuItem texto = "Inicio" href="/"/>
+        <MenuItem texto = "Personagens" href="/personagens"/>
+        <MenuItem texto = "Autores" href="/autores"/>
+        <MenuItem texto = "Séries" href="/"/>
     </Menu>
 
     <Menu Titulo = "Principais Autores">
-        <li>Teste #1</li>
-        <li>Teste #2</li>
-        <li>Teste #3</li>
+        <MenuItem texto = "J.K Rowling" href="/"/>
+        <MenuItem texto = "J.R.R Tolkien" href="/"/>
+        <MenuItem texto = "Jeorge R.R Martin" href="/autores/george-martin"/>
     </Menu>
 
     <Menu Titulo = "Outras Categorias">
-        <li>Teste #1</li>
-        <li>Teste #2</li>
-        <li>Teste #3</li>
+       <MenuItem texto = "Ultimas Noticias"/>
+       <MenuItem texto = "Séries Populares"/>
+       <MenuItem texto = "FaceBook"/>
+       <MenuItem texto = "Instagram"/>
+       <MenuItem texto = "Youtube"/>
     </Menu>
 </aside>
