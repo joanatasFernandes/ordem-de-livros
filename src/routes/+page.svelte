e3b2f4a8de4c5d4de660510b45424faf7521454d
Joanatas Fernandes Barbosa <94496092+joanatasFernandes@users.noreply.github.com>
2024-11-20 21:10:06 -0300

    Adicionando rotar para as primeiras paginas

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="flex flex-col flex justify-center items-center gap-5
+bg-black text-white min-h-screen">
+
+<h1 class="text=3xl font-bold"> Well come  the Ordem dos Livros</h1>
+<a href="/autores">Autores</a>
+</div>
