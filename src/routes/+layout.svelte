300cdb5763684e9510ca0db16d20711693ee2a2a
Joanatas Fernandes Barbosa <94496092+joanatasFernandes@users.noreply.github.com>
2024-11-20 22:02:02 -0300

    Aprendendo a usar Components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,8 @@
 	let { children } = $props();
 </script>
 
+<div class="flex flex-col flex justify-center items-center gap-5
+bg-black text-white min-h-screen">
 {@render children()}
+</div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
-<div class="flex flex-col flex justify-center items-center gap-5
-bg-black text-white min-h-screen">
+<script>
+    import Caixa from "../components/Caixa.svelte";
 
-<h1 class="text=3xl font-bold"> Well come  the Ordem dos Livros</h1>
+</script>
+
+<h1 class="text=3xl font-bold"> Ordem dos Livros</h1>
 <a href="/autores">Autores</a>
-</div>
+
+<Caixa />   
+<Caixa />   
+<Caixa />   
+<Caixa />   
+
