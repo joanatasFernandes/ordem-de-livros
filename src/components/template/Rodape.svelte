a77dd81a9bd27b80bccdc11145831a996db9b764
Joanatas Fernandes Barbosa <94496092+joanatasFernandes@users.noreply.github.com>
2025-02-26 21:29:42 -0300

    update

--- src/components/template/Rodape.svelte
+++ src/components/template/Rodape.svelte
@@ -2,6 +2,6 @@
   class="flex justify-end items-center px-20 h-16 border-t border-zinc-800"
 >
   <span class="font-thin text-zinc-400">
-    Todos os direitos reservados &copy; {new Date().getFullYear()}
+    Todos os direitos reservados &copy Joanatas Fernandes; {new Date().getFullYear()}
   </span>
 </footer>
